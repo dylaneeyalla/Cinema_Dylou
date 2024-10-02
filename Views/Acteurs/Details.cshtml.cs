561c8a742f50929299cb6086bfe8e998c99c9ffe
dylaneeyalla <120025527+dylaneeyalla@users.noreply.github.com>
2024-10-02 14:02:48 +0100

    Add files via upload

---
+++ Views/Acteurs/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TipamCinemaTicker.Views.Acteurs
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
